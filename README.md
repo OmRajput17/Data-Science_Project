1313f8787b2f89559eef5e18801eef200fc715b4
Om Rajput <rajputom2408@gmail.com>
2025-06-03 13:42:10 +0530

    Data Ingestion Pipeline

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Data-Science_Project
+# End to End Data-Science Project
+
+### Workflows in ML Pipeline\
+
+1. Data Ingestion
+2. Data Validation
+3. Data Transformation
+4. Model Trainer 
+5. Model Evaluation
+
+## Workflows
+
+1. Update config.yaml
+2. Update schema.yaml
+3. Update params.yaml
+4. Update the Entity
+5. Update the configuration manager in the src config
+6. Update the components
+7. Update the Pipeline
+9. Update the main.py
--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from src.datacience import logger
+from src.datacience.pipeline.data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom Logging.")
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<< \n\n x----------------------------x")
+    
+except Exception as e:
+    logger.exception(e)
+    raise e
